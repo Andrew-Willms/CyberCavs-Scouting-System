5523d56e10e9997b858089b38e5e58d05d729c54
Andrew-Willms <andrew.e.willms@gmail.com>
2021-11-25 04:03:56 -0500

    Added CCSS-SharedClasses project and added it as a dependency of GameMaker. Added Game.cs and GameProject.cs files and corresponding classes to CCSS-SharedClasses. Added a GameProject member to ApplicationManager.

---
+++ CyberCavsScoutingSystem/CCSS-SharedClasses/Game.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CCSS_SharedClasses {
+
+	public class Game {
+
+	}
+
+}
--- CyberCavsScoutingSystem/GameMaker/ApplicationManager.cs
+++ CyberCavsScoutingSystem/GameMaker/ApplicationManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CCSS_SharedClasses;
 
 namespace GameMaker {
 
 	public static class ApplicationManager {
+
+		private static GameProject Project;
 
 		public static void SaveGameProject() {
 
