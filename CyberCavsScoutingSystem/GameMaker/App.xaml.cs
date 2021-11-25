b8fc70ab4623621aff41372bae76bf2861d3b29a
Andrew-Willms <andrew.e.willms@gmail.com>
2021-11-25 03:41:35 -0500

    Added ApplicationStartup function to the Application class and changed set the startup of app.xaml to that function.

--- CyberCavsScoutingSystem/GameMaker/App.xaml.cs
+++ CyberCavsScoutingSystem/GameMaker/App.xaml.cs
@@ -7,9 +7,18 @@ using System.Threading.Tasks;
 using System.Windows;
 
 namespace GameMaker {
+
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application {
+
+		private void ApplicationStartup(object sender, StartupEventArgs e) {
+
+
+
+		}
+
 	}
+
 }
