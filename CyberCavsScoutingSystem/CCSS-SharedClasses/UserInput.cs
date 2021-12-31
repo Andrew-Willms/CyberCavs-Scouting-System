50ef43116958d396269b8d01296887116b73da03
Andrew-Willms <andrew.e.willms@gmail.com>
2021-12-31 03:44:32 -0500

    Changed UserInputValidatorError's Properties to have mostly private init setters (instead of pulbic init) and as such require necessary values to be passed to the (also newly added) constructor.

--- CyberCavsScoutingSystem/CCSS-SharedClasses/UserInput.cs
+++ CyberCavsScoutingSystem/CCSS-SharedClasses/UserInput.cs
@@ -1,53 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace CCSS_SharedClasses {
 
+	// A custom delegate used as the type for the InputValidator.
+	// This is necessary so the InputValidator can have an out paremeter.
+	// targetObject is an "out" parameter so this function can change UserInput.TargetObject to a new instance of T if desired.
+	public delegate void ValueConverterDelegate<T>(ref T targetObject, Dictionary<string, string> inputStrings, out List<UserInputValidationError> errors);
+
+
+
 	public class UserInput<T> {
 
+		#region Properties
 
-		public delegate T InputValidatorDelegate(string inputString, out List<UserInputValidationError> errors);
+		// This is not an auto implemented property because I need to be able to pass the object as an out parameter.
+		private T _TargetObject;
+		public T TargetObject {
+			get => _TargetObject;
+			private set => _TargetObject = value;
+		}
 
+		// I don't think this needs to be exposed publically in any way since if you know what property you are looking for
+		// you should use the "this[string propertyIdentifier]" indexer.
+		private Dictionary<string, string> InputStrings { get; init; } = new();
 
-		delegate void Del(string str);
+		public bool IsValid { get => ErrorsList.Count == 0; }
 
-		public string InputString { get; set; }
+		// Since strings are pointers to elements of a pool of strings copying Keys collection is probably safe.
+		// Editing, setting, or casting of the output array should not result in the mutation of the InputStrings dictionary.
+		private string[] PropertyNames {
+			get {
+				string[] output = new string[InputStrings.Count];
+				InputStrings.Keys.CopyTo(output, 0);
+				return output;
+			}
+		}
 
-		public T Value { get; private set; }
+		public ValueConverterDelegate<T> ValueConverter { get; private init; }
 
-		public bool IsValid { get; private set; }
+		// The errors list is "private" instead of "init" because it seems easiser to create a totally
+		// new list each time than it is to check each error remove errors from the list.
+		// Might be worth checking each error as it comes in to make sure the PropertyIdentifier is valid.
+		public List<UserInputValidationError> ErrorsList { get; private set; }
 
-		public List<UserInputValidationError> ErrorsList { get; set; }
+		#endregion Properties
 
+		#region Indexer
 
+		public string this[string propertyIdentifier] {
 
-		public InputValidatorDelegate InputValidator { get; set; }
+			// I don't think this will make the InputStrings dictionary editable by outside code.
+			get => InputStrings[propertyIdentifier];
 
+			set {
+				if (!PropertyNames.Contains(propertyIdentifier)) {
+					throw new ArgumentException($"This UserInput object does not contain the propertyIdentifier \"{propertyIdentifier}\"");
+				}
 
-		public void ValidateInput() {
+				InputStrings[propertyIdentifier] = value;
+				ValueConverter(ref _TargetObject, InputStrings, out List<UserInputValidationError> errors);
+				ErrorsList = errors;
+			}
+		}
 
-			if (InputValidator == null) {
-				throw new ArgumentNullException("The InputValidator delegate is null");
+		#endregion Indexer
+
+		#region Constructors
+
+		// If an array of initial strings is not provided create an array of empty strings of the correct size.
+		public UserInput(ValueConverterDelegate<T> valueConverter, string[] propertyNames) :
+			this(valueConverter, propertyNames, Enumerable.Repeat("", propertyNames.Length).ToArray()) { }
+
+		// If a dictionary of the property names and initial strings split it into two arrays.
+		public UserInput(ValueConverterDelegate<T> valueConverter, Dictionary<string, string> propertyNamesAndInitialStrings) :
+			this(valueConverter, propertyNamesAndInitialStrings.Keys.ToArray(), propertyNamesAndInitialStrings.Values.ToArray()) { }
+
+		public UserInput(ValueConverterDelegate<T> valueConverter, string[] propertyNames, string[] initialStrings = null) {
+
+			if (propertyNames.Length != initialStrings.Length) {
+				throw new ArgumentException("The length of the propertyNames array and the initialValues array must match.");
 			}
 
-			Value = InputValidator.Invoke(InputString, out List<UserInputValidationError> errorsList);
-			ErrorsList = errorsList;
+			ValueConverter = valueConverter;
+			InputStrings = new();
 
-			IsValid = ErrorsList.Count == 0;
+			// Create the necessary entries in the InputStrings dictionary.
+			// Since the strings are individually copied editing the source arrays or dictionary should not result in the mutation of internal data.
+			for (int i = 0; i < propertyNames.Length; i++) {
+				InputStrings.Add(PropertyNames[i], initialStrings[i]);
+			}
 		}
 
+		#endregion Constructors
 
-		public static implicit operator UserInput<T>(string inputString) {
-			return new() { InputString = inputString };
-		}
-
-		public override string ToString() {
-			return InputString;
-		}
+		// Try fancy stuff with sub properties by having generic and non generic implementations of a sub class?
 
 	}
 
