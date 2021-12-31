1501d0e658e321f557009eaece27deb22462f98f
Andrew-Willms <andrew.e.willms@gmail.com>
2021-12-31 03:44:32 -0500

    The remaineder of the last commit message with a meaningless change cause I dun-goofed and copmmited and pushed before finishing the commit message.
    Replaced UserInput class with something similar to the UserMultiInput class and removed the UserMultiInputClass. The UserInput class now has an indexer by which individual properties of the TargetValue element can be bound to and edited. There is still a single value converter to the class (something that may be changed in future) but properties of the TargetValue can be accessed individually now.
    
    Made changes to the GameEditingData class to implement the new version of the UserInput class.Changed UserInputValidatorError's Properties to have mostly private init setters (instead of pulbic init) and as such require necessary values to be passed to the (also newly added) constructor.

--- CyberCavsScoutingSystem/CCSS-SharedClasses/GameEditingData.cs
+++ CyberCavsScoutingSystem/CCSS-SharedClasses/GameEditingData.cs
@@ -8,7 +8,47 @@ namespace CCSS_SharedClasses {
 
 	public class GameEditingData {
 
-		public readonly UserInput<VersionNumber> Version = "0.0.0";
+		public GameEditingData() {
+
+			Version = new(VersionNumberValueConverter, new string[] { "MajorNumber", "MinorNumber", "PatchNumber" }, new string[] { "0", "0", "0" });
+
+			Year = new(YearValueConverter, new string[] { "" }, new string[] { "0" });
+
+		}
+
+		public readonly UserInput<VersionNumber> Version; // Cannot be initialized here because it needs to pass the instance member VersionNumberValueConverter
+
+		public void VersionNumberValueConverter(ref VersionNumber targetObject, Dictionary<string, string> inputStrings,
+			out List<UserInputValidationError> errors) {
+
+			errors = new();
+			Dictionary<string, int> versionComponents = new();
+
+			foreach (KeyValuePair<string, string> input in inputStrings) {
+
+				string invalidCharacteres = string.Concat(input.Value.Where(x => char.IsDigit(x) == false));
+
+				if (invalidCharacteres.Length > 0) {
+					errors.Add(new("Invalid Characters", UserInputValidationErrorSeverity.Error, input.Key)); // somehow need to make a tooltip too
+
+				} else {
+					versionComponents.Add(input.Key, int.Parse(input.Value));
+				}
+			}
+
+			if (errors.Count == 0) {
+
+				// Since there is no other state attached to VersionNumber and since VersionNumber is a small class it's easiest just to
+				// instantiate a new object.
+				targetObject = new(versionComponents["MajorNumber"], versionComponents["MinorNumber"], versionComponents["PatchNumber"]);
+
+			} else {
+				targetObject = null;
+			}
+
+			return;
+		}
+
 		public string VersionDescription = "";
 		public DateTime VersionReleaseDate;
 
@@ -18,10 +58,23 @@ namespace CCSS_SharedClasses {
 		public string Name = "";
 		public string Description = "";
 
-		private readonly UserInput<int> Year = "0";
-		//public UserInputBindingData<int> Year { get => _Year; } // this is how to do readonly properties (they can't be auto implemented)
+		public readonly UserInput<int> Year;
 
+		public void YearValueConverter(ref int targetObject, Dictionary<string, string> inputStrings, out List<UserInputValidationError> errors) {
 
+			errors = new();
+
+			string invalidCharacteres = string.Concat(inputStrings[""].Where(x => char.IsDigit(x) == false));
+
+			if (invalidCharacteres.Length > 0) {
+				errors.Add(new("Invalid Characters", UserInputValidationErrorSeverity.Error, "")); // somehow need to make a tooltip too
+				targetObject = 0; // Since it is a value type that is not nullable the best I can do is return the default value.
+
+			} else {
+				targetObject = int.Parse(inputStrings[""]);
+			}
+
+		}
 
 	}
 
--- CyberCavsScoutingSystem/CCSS-SharedClasses/UserInputValidationError.cs
+++ CyberCavsScoutingSystem/CCSS-SharedClasses/UserInputValidationError.cs
@@ -9,19 +9,28 @@ namespace CCSS_SharedClasses {
 	// I think these things need to be static
 	public record UserInputValidationError {
 
-		public string Name { get; init; }
+		public string Name { get; private init; }
+
+		public UserInputValidationErrorSeverity Severity { get; private init; }
 
 		// This can be used to provide additional identifying information about the error.
 		// For example, if multi data binding is used to bind multiple View elements to a single UserMultiInput class
 		// the Identifier parameter can be used to indicate which of the multiple view elements the error relates to.
-		public string Identifier { get; init; }
+		public string Identifier { get; private init; }
 
 		public object Tooltip { get; init; }
 
-		public UserInputValidationErrorSeverity Severity { get; set; }
-
 		//public sometingHere ToolTipStyle { get; init; }
+
+		public UserInputValidationError(string name, UserInputValidationErrorSeverity severity, string identifier) {
+			Name = name;
+			Severity = severity;
+			Identifier = identifier;
+		}
+
 	}
+
+	// A meaningless change
 
 	public enum UserInputValidationErrorSeverity {
 		Note = 0,
