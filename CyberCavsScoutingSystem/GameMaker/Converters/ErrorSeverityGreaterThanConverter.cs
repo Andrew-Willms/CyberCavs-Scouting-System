adcc3bb3d48380837ea4ae7fdd8806b1710cbd9c
Andrew-Willms <andrew.e.willms@gmail.com>
2022-03-14 01:19:14 -0400

    Removed commented out implementation of EnumGreaterThanConverter from ErrorSeverityGreaterThanConverter.cs.

--- CyberCavsScoutingSystem/GameMaker/Converters/ErrorSeverityGreaterThanConverter.cs
+++ CyberCavsScoutingSystem/GameMaker/Converters/ErrorSeverityGreaterThanConverter.cs
@@ -6,30 +6,4 @@ using CCSSDomain;
 
 namespace GameMaker.Converters;
 
-//public class EnumGreaterThanConverter<TEnum> : IValueConverter where TEnum : Enum {
-
-//	public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-
-//		TEnum threshold, severity;
-
-//		if (value is TEnum valueAsTEnum) {
-//			severity = valueAsTEnum;
-//		} else {
-//			throw new ArgumentException($"The parameter \"{nameof(value)}\" cannot be converted to a StringInputValidationErrorSeverity");
-//		}
-
-//		if (parameter is TEnum parameterAsTEnum) {
-//			threshold = parameterAsTEnum;
-//		} else {
-//			throw new ArgumentException($"The parameter \"{nameof(parameter)}\" cannot be converted to a StringInputValidationErrorSeverity");
-//		}
-
-//		return (severity.CompareTo(threshold) > 0);
-//	}
-
-//	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-//		throw new NotImplementedException("This conversion does not work in reverse.");
-//	}
-//}
-
 public class ErrorGreaterThanConverter : WPFUtilities.EnumGreaterThanConverter<CCSSDomain.ErrorSeverity> { }
