eaa4ee16ce4d1cfeebbcac430ed40ea1b65835bb
Andrew-Willms <andrew.e.willms@gmail.com>
2022-05-01 22:29:08 -0400

    Formatting changes.

--- CyberCavsScoutingSystem/WPFUtilities/SmartEnum.cs
+++ CyberCavsScoutingSystem/WPFUtilities/SmartEnum.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reflection;
 
 namespace WPFUtilities; 
 
