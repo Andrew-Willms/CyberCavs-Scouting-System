eebd1a138ce870a4b22beb2db9d6eacf1e6301f6
Andrew-Willms <andrew.e.willms@gmail.com>
2022-06-29 02:22:57 -0400

    Updated GameProject to use GameEditingData.GetDefaultEditingData() instead of the now-private constructor.
    Other formatting changes.

--- CyberCavsScoutingSystem/CCSSDomain/GameProject.cs
+++ CyberCavsScoutingSystem/CCSSDomain/GameProject.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CCSSDomain;
 
-namespace CCSSDomain;
+
 
 public class GameProject {
 
 	// Do I even need this or should this be generated later?
 	public Game Game = new();
 
-	public GameEditingData EditingData = new();
+	public GameEditingData EditingData = GameEditingData.GetDefaultEditingData();
 
 	// Maybe have an edit history here too, work it out later.
 
