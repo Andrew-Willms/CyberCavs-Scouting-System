1ed1c69db90db72b5120631513cc57a7428158b4
Andrew-Willms <andrew.e.willms@gmail.com>
2022-06-29 00:00:38 -0400

    Made the ValidationEvent and all corresponding parameters of all ValidationSet implementation non-nullable.

--- CyberCavsScoutingSystem/WPFUtilities/Validation/ValidationSet.cs
+++ CyberCavsScoutingSystem/WPFUtilities/Validation/ValidationSet.cs
@@ -7,52 +7,107 @@ namespace WPFUtilities.Validation;
 public interface IValidationSet<in TTargetType, TSeverityEnum> 
 	where TSeverityEnum : ValidationErrorSeverityEnum<TSeverityEnum>, IValidationErrorSeverityEnum<TSeverityEnum> {
 
-	public IValidationTrigger ToValidationTrigger(ValidationEvent validationEvent, Func<TTargetType> targetObjectGetter,
+	public IValidationTrigger ToValidationTrigger(Func<TTargetType> targetObjectGetter,
 		Action<ValidationError<TSeverityEnum>> postValidationAction);
 }
+
+
 
 public class ValidationSet<TTargetType, TSeverityEnum> : IValidationSet<TTargetType, TSeverityEnum>
 	where TSeverityEnum : ValidationErrorSeverityEnum<TSeverityEnum>, IValidationErrorSeverityEnum<TSeverityEnum> {
 
 	private StringInputValidator<TTargetType, TSeverityEnum> Validator { get; }
 
-	private ValidationEvent? ValidationEvent { get; }
+	private ValidationEvent ValidationEvent { get; }
 
-	public ValidationSet(StringInputValidator<TTargetType, TSeverityEnum> validator, ValidationEvent? validationEvent = null) {
+	public ValidationSet(StringInputValidator<TTargetType, TSeverityEnum> validator, ValidationEvent validationEvent) {
 
 		Validator = validator;
 		ValidationEvent = validationEvent;
 	}
 
-	public IValidationTrigger ToValidationTrigger(ValidationEvent validationEvent, Func<TTargetType> targetObjectGetter,
+	public IValidationTrigger ToValidationTrigger(Func<TTargetType> targetObjectGetter,
 		Action<ValidationError<TSeverityEnum>> postValidationAction) {
 
-		return new ValidationTrigger<TTargetType, TSeverityEnum>(Validator, ValidationEvent ?? validationEvent,
+		return new ValidationTrigger<TTargetType, TSeverityEnum>(Validator, ValidationEvent,
 			targetObjectGetter, postValidationAction);
 	}
 }
+
+
 
 public class ValidationSet<TTargetType, TValidationParameter, TSeverityEnum> : IValidationSet<TTargetType, TSeverityEnum>
 	where TSeverityEnum : ValidationErrorSeverityEnum<TSeverityEnum>, IValidationErrorSeverityEnum<TSeverityEnum> {
 
 	private StringInputValidator<TTargetType, TValidationParameter, TSeverityEnum> Validator { get; }
 
-	private ValidationEvent? ValidationEvent { get; }
+	private ValidationEvent ValidationEvent { get; }
 
 	private Func<TValidationParameter> ValidationParameterGetter { get; }
 
 	public ValidationSet(StringInputValidator<TTargetType, TValidationParameter, TSeverityEnum> validator,
-		Func<TValidationParameter> validationParameterGetter, ValidationEvent? validationEvent = null) {
+		Func<TValidationParameter> validationParameterGetter, ValidationEvent validationEvent) {
 
 		Validator = validator;
 		ValidationEvent = validationEvent;
 		ValidationParameterGetter = validationParameterGetter;
 	}
 
-	public IValidationTrigger ToValidationTrigger(ValidationEvent validationEvent, Func<TTargetType> targetObjectGetter,
+	public IValidationTrigger ToValidationTrigger(Func<TTargetType> targetObjectGetter,
 		Action<ValidationError<TSeverityEnum>> postValidationAction) {
 
-		return new ValidationTrigger<TTargetType, TValidationParameter, TSeverityEnum>(Validator, ValidationEvent ?? validationEvent,
+		return new ValidationTrigger<TTargetType, TValidationParameter, TSeverityEnum>(Validator, ValidationEvent,
+			targetObjectGetter, ValidationParameterGetter, postValidationAction);
+	}
+}
+
+
+
+
+public class CovalidationSet<TTargetType, TSeverityEnum> : IValidationSet<TTargetType, TSeverityEnum>
+	where TSeverityEnum : ValidationErrorSeverityEnum<TSeverityEnum>, IValidationErrorSeverityEnum<TSeverityEnum> {
+
+	private MultiStringInputCovalidator<TTargetType, TSeverityEnum> Validator { get; }
+
+	private ValidationEvent ValidationEvent { get; }
+
+	public CovalidationSet(MultiStringInputCovalidator<TTargetType, TSeverityEnum> validator, ValidationEvent validationEvent) {
+
+		Validator = validator;
+		ValidationEvent = validationEvent;
+	}
+
+	public IValidationTrigger ToValidationTrigger(Func<TTargetType> targetObjectGetter,
+		Action<ValidationError<TSeverityEnum>> postValidationAction) {
+
+		return new CovalidationTrigger<TTargetType, TSeverityEnum>(Validator, ValidationEvent,
+			targetObjectGetter, postValidationAction);
+	}
+}
+
+
+
+public class CovalidationSet<TTargetType, TValidationParameter, TSeverityEnum> : IValidationSet<TTargetType, TSeverityEnum>
+	where TSeverityEnum : ValidationErrorSeverityEnum<TSeverityEnum>, IValidationErrorSeverityEnum<TSeverityEnum> {
+
+	private StringInputValidator<TTargetType, TValidationParameter, TSeverityEnum> Validator { get; }
+
+	private ValidationEvent ValidationEvent { get; }
+
+	private Func<TValidationParameter> ValidationParameterGetter { get; }
+
+	public CovalidationSet(StringInputValidator<TTargetType, TValidationParameter, TSeverityEnum> validator,
+		Func<TValidationParameter> validationParameterGetter, ValidationEvent validationEvent) {
+
+		Validator = validator;
+		ValidationEvent = validationEvent;
+		ValidationParameterGetter = validationParameterGetter;
+	}
+
+	public IValidationTrigger ToValidationTrigger(Func<TTargetType> targetObjectGetter,
+		Action<ValidationError<TSeverityEnum>> postValidationAction) {
+
+		return new ValidationTrigger<TTargetType, TValidationParameter, TSeverityEnum>(Validator, ValidationEvent,
 			targetObjectGetter, ValidationParameterGetter, postValidationAction);
 	}
 }
