31cff2423a88ecf5782f8f99e8950238513dc56c
Andrew-Willms <andrew.e.willms@gmail.com>
2022-06-29 02:20:13 -0400

    Added method overload to Math.Max.

--- CyberCavsScoutingSystem/WPFUtilities/Math.cs
+++ CyberCavsScoutingSystem/WPFUtilities/Math.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WPFUtilities; 
 
@@ -9,6 +11,15 @@ public static class Math {
 	public static T Max<T>(T left, T right) where T : IComparable {
 
 		return left.CompareTo(right) > 0 ? left : right;
+	}
+
+	public static T Max<T>(params T[] items) where T : IComparable {
+
+		if (items.Length == 0) {
+			throw new ArgumentException("");
+		}
+
+		return items.Max() ?? throw new NullReferenceException("Somehow the max is null.");
 	}
 
 }
