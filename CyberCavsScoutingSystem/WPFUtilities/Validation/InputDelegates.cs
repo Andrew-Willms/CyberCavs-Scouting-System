3ab2854aa81220c319973d0dfc3f37cd62e491c8
Andrew-Willms <andrew.e.willms@gmail.com>
2022-07-01 16:38:02 -0400

    Added comments to InputDelegates.cs

--- CyberCavsScoutingSystem/WPFUtilities/Validation/InputDelegates.cs
+++ CyberCavsScoutingSystem/WPFUtilities/Validation/InputDelegates.cs
@@ -35,3 +35,9 @@ public delegate ValidationError<TSeverityEnum>?
 	MultiInputValidator<in TTargetType, in TValidationParameter, TSeverityEnum>
 	(TTargetType targetObject, TValidationParameter parameter) 
 	where TSeverityEnum : ValidationErrorSeverityEnum<TSeverityEnum>, IValidationErrorSeverityEnum<TSeverityEnum>;
+
+
+/*
+When value types are used in StringInputValidator<TheValueType, TSeverityEnum> the method cannot return a nullable
+version of that value type because C# handles nullable reference types and nullable value types differently
+*/
