4c0fccdd4195d1d90c4a1738463fb29f7797a94e
Andrew-Willms <andrew.e.willms@gmail.com>
2022-07-05 21:32:07 -0400

    Added constructors to ConversionPair to take any combination of single error or error list converters or inverters.

--- CyberCavsScoutingSystem/WPFUtilities/Validation/Delegates/ConversionPairs.cs
+++ CyberCavsScoutingSystem/WPFUtilities/Validation/Delegates/ConversionPairs.cs
@@ -11,6 +11,14 @@ public record ConversionPair<TOutput, TInput, TSeverityEnum>(
 	where TSeverityEnum : ValidationErrorSeverityEnum<TSeverityEnum>, IValidationErrorSeverityEnum<TSeverityEnum> {
 	
 	public ConversionPair(SingleInputConverterSingleError<TOutput?, TInput, TSeverityEnum> converter,
+		SingleInputInverterErrorList<TOutput, TInput?, TSeverityEnum> inverter)
+		: this(DelegateConverters.SingleToErrorListConvert(converter), inverter) { }
+
+	public ConversionPair(SingleInputConverterErrorList<TOutput?, TInput, TSeverityEnum> converter,
+		SingleInputInverterSingleError<TOutput, TInput?, TSeverityEnum> inverter)
+		: this(converter, DelegateConverters.SingleToErrorListInvert(inverter)) { }
+
+	public ConversionPair(SingleInputConverterSingleError<TOutput?, TInput, TSeverityEnum> converter,
 		SingleInputInverterSingleError<TOutput, TInput?, TSeverityEnum> inverter)
 		: this(DelegateConverters.SingleToErrorListConvert(converter),
 			DelegateConverters.SingleToErrorListInvert(inverter)) { }
