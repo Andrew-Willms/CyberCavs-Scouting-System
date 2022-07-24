7b88095da1fa5a4cac416b10c4691ec339fe289a
Andrew-Willms <andrew.e.willms@gmail.com>
2022-07-24 04:18:48 -0400

    Added Data folder to GameMaker

---
+++ CyberCavsScoutingSystem/GameMaker/Data/MetaTabData.cs
@@ -0,0 +1,13 @@
+﻿namespace GameMaker.Data; 
+
+public static class MetaTabData {
+
+	public static string NameText { get; } = "Name";
+
+	public static string YearText = "Year";
+
+	public static string VersionText = "Version";
+
+	public static string VersionDescriptionText { get; } = "Version Description";
+
+}
