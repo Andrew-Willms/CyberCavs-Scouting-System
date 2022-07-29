a53d7d1792957ec03981f6d843fbbc3946f41ab3
Andrew-Willms <andrew.e.willms@gmail.com>
2022-07-29 02:36:53 -0400

    Fixed Tests so that they actually work as intended.

--- CyberCavsScoutingSystem/CCSSDomain.Tests/GameValidator/Name.cs
+++ CyberCavsScoutingSystem/CCSSDomain.Tests/GameValidator/Name.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
+using Xunit;
 using WPFUtilities;
 using WPFUtilities.Validation;
-using WPFUtilities.Validation.Errors;
-using Xunit;
+using Error = WPFUtilities.Validation.Errors.ValidationError<CCSSDomain.ErrorSeverity>;
 
 namespace CCSSDomain.Tests.GameValidator;
 
@@ -10,61 +10,83 @@ namespace CCSSDomain.Tests.GameValidator;
 
 public class Name {
 
-	[Fact]
-	public void NullNameShouldThrowException() {
+	public class Converter {
 
-		Assert.Throws<NullInputObjectInConverterException>(() => 
-			Validation.Validators.GameTextValidator.NameConversionPair.Converter(null!));
+		[Fact]
+		public void NullNameShouldThrowException() {
+
+			Assert.Throws<NullInputObjectInConverterException>(() => 
+				Validation.Validators.GameTextValidator.NameConversionPair.Converter(null!));
+		}
+
+		[Theory]
+		[InlineData("test")]
+		public void ValidNameShouldEcho(string validName) {
+
+			(Optional<string> optional, ReadOnlyList<Error> errors)
+				= Validation.Validators.GameTextValidator.NameConversionPair.Converter(validName);
+
+			Assert.True(optional.HasValue);
+			Assert.Empty(errors);
+			Assert.Equal(optional.Value, validName);
+		}
+
 	}
 
-	[Fact]
-	public void EmptyNameShouldError() {
+	public class Inverter {
 
-		(Optional<string> optional, ReadOnlyList<ValidationError<ErrorSeverity>> errors)
-			= Validation.Validators.GameTextValidator.NameConversionPair.Converter("");
+		[Fact]
+		public void NullNameShouldThrowException() {
 
-		Assert.False(optional.HasValue);
-		Assert.True(errors.Count == 1);
-		Assert.Equal(errors[0], Validation.Data.GameValidationData.Name.Length.TooLongError);
+			Assert.Throws<NullInputObjectInInverterException>(() => 
+				Validation.Validators.GameTextValidator.NameConversionPair.Inverter(null!));
+		}
+
+		[Theory]
+		[InlineData("test")]
+		public void ValidNameShouldEcho(string validName) {
+
+			(Optional<string> optional, ReadOnlyList<Error> errors)
+				= Validation.Validators.GameTextValidator.NameConversionPair.Inverter(validName);
+
+			Assert.True(optional.HasValue);
+			Assert.Empty(errors);
+			Assert.Equal(optional.Value, validName);
+		}
+
 	}
 
-	[Theory]
-	[InlineData("test")]
-	public void ValidNameShouldEcho(string validName) {
+	public class LengthValidator {
 
-		(Optional<string> optional, ReadOnlyList<ValidationError<ErrorSeverity>> errors)
-			= Validation.Validators.GameTextValidator.NameConversionPair.Converter(validName);
+		public static IEnumerable<object[]> LongNameShouldAdviseTestData() {
 
-		Assert.True(optional.HasValue);
-		Assert.Empty(errors);
-		Assert.Equal(optional.Value, validName);
+			yield return new object[] {
+				"Some Really Long Name One Two Three"
+			};
+		}
+
+		[Fact]
+		public void EmptyNameShouldError() {
+
+			Optional<Error> error = Validation.Validators.GameTextValidator.NameValidator_Length("");
+
+			Assert.True(error.HasValue);
+			Assert.Equal(error.Value, Validation.Data.GameValidationData.Name.Length.TooShortError);
+		}
+
+
+		[Theory]
+		[MemberData(nameof(LongNameShouldAdviseTestData))]
+		public void LongNameShouldAdvise(string longName) {
+
+			Optional<Error> error = Validation.Validators.GameTextValidator.NameValidator_Length(longName);
+
+			Assert.True(error.HasValue);
+			Assert.Equal(error.Value, Validation.Data.GameValidationData.Name.Length.TooLongAdvisory);
+		}
+
 	}
-
-
 
 	// TODO: probably right more tests, IDK if I should have one for each error level hard coded or have logic to test.
-
-
-
-	[Theory]
-	[MemberData(nameof(LongNameShouldAdviseTestData))]
-	public void LongNameShouldAdvise(string longName) {
-
-		(Optional<string> optional, ReadOnlyList<ValidationError<ErrorSeverity>> errors)
-			= Validation.Validators.GameTextValidator.NameConversionPair.Converter(longName);
-
-		Assert.True(optional.HasValue);
-		Assert.Equal(optional.Value, longName);
-		Assert.True(errors.Count == 1);
-		Assert.Equal(errors[0], Validation.Data.GameValidationData.Name.Length.TooLongAdvisory);
-	}
-
-	public static IEnumerable<object[]> LongNameShouldAdviseTestData() {
-
-		yield return new object[] {
-			"Some Really Long Name One Two Three Four Five Six Seven",
-			"Some Really Long Name One Two Three Four Five Six Seven Eight",
-		};
-	}
 
 }
