8dde669c962ae4bc5e19c0bd7cbb18bb826963b0
Evan Armoogan <evanjarmoogan@gmail.com>
2022-08-15 20:41:11 -0400

    Added scouting app project.

---
+++ CyberCavsScoutingSystem/ScoutingApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ScoutingApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CyberCavsScoutingSystem/ScoutingApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ScoutingApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
