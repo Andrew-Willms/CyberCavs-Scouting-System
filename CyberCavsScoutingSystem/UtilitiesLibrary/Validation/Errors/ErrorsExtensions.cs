23917717bee60c4661ad076c5daa9bdebca9c4de
Andrew-Willms <andrew.e.willms@gmail.com>
2022-08-28 16:11:15 -0400

    Removed unused using statements including one causing the GitHub CI job to fail.

--- CyberCavsScoutingSystem/UtilitiesLibrary/Validation/Errors/ErrorsExtensions.cs
+++ CyberCavsScoutingSystem/UtilitiesLibrary/Validation/Errors/ErrorsExtensions.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Controls;
-
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace UtilitiesLibrary.Validation.Errors; 
 
