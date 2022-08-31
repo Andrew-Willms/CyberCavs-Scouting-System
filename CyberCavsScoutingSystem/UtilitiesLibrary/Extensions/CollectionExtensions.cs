af116fa2b4acbfd376a4bb542ec2b84da599a4f6
Andrew-Willms <andrew.e.willms@gmail.com>
2022-08-31 14:55:06 -0400

    Changed Game class to have init only properties.
    Added SelectIfNotNull and SelectIfHasValue collection extensions.
    
    Changed GameMakerWpf to the preview LangVersion

--- CyberCavsScoutingSystem/UtilitiesLibrary/Extensions/CollectionExtensions.cs
+++ CyberCavsScoutingSystem/UtilitiesLibrary/Extensions/CollectionExtensions.cs
@@ -34,10 +34,28 @@ public static class CollectionExtensions {
 		return newValue is null ? enumerable : enumerable.Append(newValue);
 	}
 
+	public static IEnumerable<TTarget?> SelectIfNotNull<TCollection, TTarget>
+		(this IEnumerable<TCollection> enumerable, Func<TCollection, TTarget?> selector) {
+
+		return enumerable.Select(selector).Where(x => x is not null);
+	}
+
+	public static IEnumerable<TTarget> SelectIfHasValue<TCollection, TTarget>
+		(this IEnumerable<TCollection> enumerable, Func<TCollection, Optional<TTarget>> selector) {
+
+		return enumerable.Where(x => selector(x).HasValue).Select(x => selector(x).Value);
+
+		//return enumerable.Select(selector).Where(x => x.HasValue).Select(x => x.Value);
+	}
+
+
+
 	public static ReadOnlyList<T> ReadOnlyListify<T>(this T item) {
 
 		return new List<T> { item }.ToReadOnly();
 	}
+
+
 
 	public static bool OnlyOne<T>(this IEnumerable<T> enumerable, T value) where T : IComparable {
 
