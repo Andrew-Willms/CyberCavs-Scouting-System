c4e6742d6c4283a446ad2dba3fd41bd04180ffe5
Andrew-Willms <andrew.e.willms@gmail.com>
2022-10-01 22:30:02 -0400

    Fixed but where the Remove Alliance button could be clicked even when an alliance wasn't selected.

--- CyberCavsScoutingSystem/GameMakerWpf/Views/AlliancesTabView.xaml.cs
+++ CyberCavsScoutingSystem/GameMakerWpf/Views/AlliancesTabView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows.Controls;
 using System.Collections.ObjectModel;
 using CCSSDomain;
@@ -16,20 +17,25 @@ public partial class AlliancesTabView : UserControl, INotifyPropertyChanged {
 
 	public static GameEditingData GameEditingData => ApplicationManager.Game;
 
-	public AlliancesTabView() {
-
-		DataContext = this;
-
-		InitializeComponent();
-	}
-
 	private int _SelectedAllianceIndex = -1;
 	public int SelectedAllianceIndex {
 		get => _SelectedAllianceIndex;
 		set {
 			_SelectedAllianceIndex = value;
 			OnPropertyChanged(nameof(SelectedAllianceIndex));
+			OnPropertyChanged(nameof(RemoveButtonIsEnabled));
 		}
+	}
+
+	public bool RemoveButtonIsEnabled => SelectedAllianceIndex != -1;
+
+
+
+	public AlliancesTabView() {
+
+		DataContext = this;
+
+		InitializeComponent();
 	}
 
 
@@ -39,6 +45,7 @@ public partial class AlliancesTabView : UserControl, INotifyPropertyChanged {
 	public SingleInput<uint, string, ErrorSeverity> RobotsPerAlliance => ApplicationManager.Game.RobotsPerAlliance;
 
 
+
 	private void AddButton_Click(object sender, System.Windows.RoutedEventArgs e) {
 		ApplicationManager.AddAlliance();
 	}
@@ -46,6 +53,15 @@ public partial class AlliancesTabView : UserControl, INotifyPropertyChanged {
 	private void RemoveButton_Click(object sender, System.Windows.RoutedEventArgs e) {
 		ApplicationManager.RemoveAlliance(GameEditingData.Alliances[SelectedAllianceIndex]);
 	}
+
+	private void MoveUpButton_Click(object sender, System.Windows.RoutedEventArgs e) {
+		throw new NotImplementedException();
+	}
+
+	private void MoveDownButton_Click(object sender, System.Windows.RoutedEventArgs e) {
+		throw new NotImplementedException();
+	}
+
 
 
 
