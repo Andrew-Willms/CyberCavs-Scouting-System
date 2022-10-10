445a56de4c9965fa3b97f7c5132a73eabc7a3175
Andrew-Willms <andrew.e.willms@gmail.com>
2022-10-10 00:36:01 -0400

    Removed unecessary parentheses.

--- CyberCavsScoutingSystem/GameMakerWpf/Converters/ErrorToBrushConverters.cs
+++ CyberCavsScoutingSystem/GameMakerWpf/Converters/ErrorToBrushConverters.cs
@@ -11,7 +11,7 @@ namespace GameMakerWpf.Converters;
 public class ErrorToToolTipTextBrushConverter : IErrorConverter<ErrorSeverity, Brush> {
 
 	public ReadOnlyDictionary<ErrorSeverity, Brush> ConversionDictionary { get; } =
-		new(new Dictionary<ErrorSeverity, Brush>() {
+		new(new Dictionary<ErrorSeverity, Brush> {
 			{ ErrorSeverity.None, Brushes.Blue }, // this shouldn't be used but is here to prevent null exceptions and help with debugging
 			{ ErrorSeverity.Info, Brushes.Gray },
 			{ ErrorSeverity.Advisory, Brushes.Gold },
@@ -25,7 +25,7 @@ public class ErrorToNormalBrushConverter : IErrorConverter<ErrorSeverity, Brush>
 	private static readonly SolidColorBrush NoneNormalBrush = new((Color)ColorConverter.ConvertFromString("#FFABADD3"));
 
 	public ReadOnlyDictionary<ErrorSeverity, Brush> ConversionDictionary { get; } =
-		new(new Dictionary<ErrorSeverity, Brush>() {
+		new(new Dictionary<ErrorSeverity, Brush> {
 			{ ErrorSeverity.None, NoneNormalBrush },
 			{ ErrorSeverity.Info, Brushes.Gray },
 			{ ErrorSeverity.Advisory, Brushes.Gold },
@@ -39,7 +39,7 @@ public class ErrorToMouseOverBrushConverter : IErrorConverter<ErrorSeverity, Bru
 	private static readonly SolidColorBrush NoneMouseOverBrush = new((Color)ColorConverter.ConvertFromString("#FF7EB4EA"));
 
 	public ReadOnlyDictionary<ErrorSeverity, Brush> ConversionDictionary { get; } =
-		new(new Dictionary<ErrorSeverity, Brush>() {
+		new(new Dictionary<ErrorSeverity, Brush> {
 			{ ErrorSeverity.None, NoneMouseOverBrush },
 			{ ErrorSeverity.Info, Brushes.Gray },
 			{ ErrorSeverity.Advisory, Brushes.Gold },
@@ -53,7 +53,7 @@ public class ErrorToFocusedBrushConverter : IErrorConverter<ErrorSeverity, Brush
 	private static readonly SolidColorBrush NoneFocusedBrush = new((Color)ColorConverter.ConvertFromString("#FF569DE5"));
 
 	public ReadOnlyDictionary<ErrorSeverity, Brush> ConversionDictionary { get; } =
-		new(new Dictionary<ErrorSeverity, Brush>() {
+		new(new Dictionary<ErrorSeverity, Brush> {
 			{ ErrorSeverity.None, NoneFocusedBrush },
 			{ ErrorSeverity.Info, Brushes.Gray },
 			{ ErrorSeverity.Advisory, Brushes.Gold },
