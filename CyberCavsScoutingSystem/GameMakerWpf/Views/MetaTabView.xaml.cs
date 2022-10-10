19e671242de9a68750c2b46db6460981b54a03ef
Andrew-Willms <andrew.e.willms@gmail.com>
2022-10-10 00:37:10 -0400

    Changed MetaTabView to use itself as a data context instead of a GameEditingData

--- CyberCavsScoutingSystem/GameMakerWpf/Views/MetaTabView.xaml.cs
+++ CyberCavsScoutingSystem/GameMakerWpf/Views/MetaTabView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows.Controls;
+using CCSSDomain;
+using GameMakerWpf.Domain;
+using UtilitiesLibrary.Validation.Inputs;
 
 namespace GameMakerWpf.Views;
 
@@ -6,7 +9,19 @@ namespace GameMakerWpf.Views;
 
 public partial class MetaTabView : UserControl {
 
+	private static GameEditingData GameEditingData => ApplicationManager.GameEditingData;
+
+	public static SingleInput<string, string, ErrorSeverity> GameName => GameEditingData.Name;
+	public static SingleInput<string, string, ErrorSeverity> Description => GameEditingData.Description;
+	public static SingleInput<int, string, ErrorSeverity> Year => GameEditingData.Year;
+	public static MultiInput<Version, ErrorSeverity, uint, uint, uint, string> Version => GameEditingData.Version;
+
+
+
 	public MetaTabView() {
+
+		DataContext = this;
+
 		InitializeComponent();
 	}
 
