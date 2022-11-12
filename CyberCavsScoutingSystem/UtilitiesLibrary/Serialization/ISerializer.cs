f11131a134aa655758a8de30a4384e3b545bc07f
Andrew-Willms <andrew.e.willms@gmail.com>
2022-11-12 00:54:28 -0500

    Created ISerializer interface.

---
+++ CyberCavsScoutingSystem/UtilitiesLibrary/Serialization/ISerializer.cs
@@ -0,0 +1,11 @@
+﻿namespace UtilitiesLibrary.Serialization; 
+
+
+
+public interface ISerializable<T, out TSelf> where TSelf : ISerializable<T, TSelf> {
+
+	public static abstract TSelf ToSerializable(T value);
+
+	public T FromSerializable();
+
+}
