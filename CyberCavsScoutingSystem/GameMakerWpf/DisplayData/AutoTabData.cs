180ead84a3fe26ba7554e82cfd32c8b114d32e3e
Andrew-Willms <andrew.e.willms@gmail.com>
2022-11-23 22:46:41 -0500

    Added column headers in AutoTab.

--- CyberCavsScoutingSystem/GameMakerWpf/DisplayData/AutoTabData.cs
+++ CyberCavsScoutingSystem/GameMakerWpf/DisplayData/AutoTabData.cs
@@ -8,6 +8,20 @@ public class AutoTabData {
 
 	public static string ButtonsText => "Buttons";
 
+	public static string ButtonDataFieldNameText => "Data Field";
+
+	public static string ButtonTextText => "Button Text";
+
+	public static string ButtonIncrementAmountText => "Increment";
+
+	public static string ButtonXPositionText => "X Position";
+
+	public static string ButtonYPositionText => "Y Position";
+
+	public static string ButtonWidthText => "Width";
+
+	public static string ButtonHeightText => "Height";
+
 	public static string AddText => "Add";
 
 	public static string RemoveText => "Remove";
