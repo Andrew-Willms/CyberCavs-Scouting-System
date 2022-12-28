e28931a4ab4568d05db1401e7aa9df0a0d12ecc6
Andrew-Willms <andrew.e.willms@gmail.com>
2022-12-28 16:10:28 -0500

    Added ScoutingApp Maui project.

---
+++ CyberCavsScoutingSystem/ScoutingApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,7 @@
+﻿using Foundation;
+
+namespace ScoutingApp;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate {
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
