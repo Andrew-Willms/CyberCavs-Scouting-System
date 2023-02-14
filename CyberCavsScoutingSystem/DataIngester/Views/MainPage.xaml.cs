53dc7b43c1cccd2e58d16126290c778a45487884
Andrew-Willms <andrew.e.willms@gmail.com>
2023-02-14 02:07:31 -0500

    Created DataIngester.

---
+++ CyberCavsScoutingSystem/DataIngester/Views/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Microsoft.Maui.Controls;
+
+namespace DataIngester.Views;
+
+
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged {
+
+	private FileViewModel _TargetFile = new() { };
+	public FileViewModel TargetFile {
+		get => _TargetFile;
+		set {
+			_TargetFile = value;
+			OnPropertyChanged(nameof(TargetFile));
+		}
+	}
+
+	public ObservableCollection<FileViewModel> SourceFiles { get; } = new() {
+		new() { Path = "path 1" },
+		new() { Path = "path 2" },
+		new() { Path = "path 3" }
+	};
+
+	public ObservableCollection<string> LogMessages { get; } = new();
+
+	public MainPage() {
+		InitializeComponent();
+		BindingContext = this;
+	}
+
+
+	public new event PropertyChangedEventHandler? PropertyChanged;
+
+	private new void OnPropertyChanged(string propertyName) {
+		PropertyChanged?.Invoke(this, new(propertyName));
+	}
+
+}
