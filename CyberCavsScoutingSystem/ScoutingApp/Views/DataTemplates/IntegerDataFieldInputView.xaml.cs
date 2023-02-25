272f2cad7da4b2dcd3fd69260d70d28631e63981
Andrew-Willms <andrew.e.willms@gmail.com>
2023-02-24 22:32:12 -0500

    Removed CounterView and moved the contents of CounterView into IntegerDataFieldInputView

--- CyberCavsScoutingSystem/ScoutingApp/Views/DataTemplates/IntegerDataFieldInputView.xaml.cs
+++ CyberCavsScoutingSystem/ScoutingApp/Views/DataTemplates/IntegerDataFieldInputView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Maui.Controls;
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Maui.Controls;
+using IntegerDataField = CCSSDomain.DataCollectors.IntegerDataField;
 
 namespace ScoutingApp.Views.DataTemplates; 
 
@@ -8,6 +11,18 @@ public partial class IntegerDataFieldInputView : ContentView {
 
 	public IntegerDataFieldInputView() {
 		InitializeComponent();
+	}
+
+	private void IncrementButton_OnClick(object? sender, EventArgs e) {
+
+		IntegerDataField dataField = BindingContext as IntegerDataField ?? throw new InvalidOperationException();
+		dataField.Value++;
+	}
+
+	private void DecrementButton_OnClicked(object? sender, EventArgs e) {
+
+		IntegerDataField dataField = BindingContext as IntegerDataField ?? throw new InvalidOperationException();
+		dataField.Value--;
 	}
 
 }
