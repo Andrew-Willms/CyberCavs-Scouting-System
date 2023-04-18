234de638bc9d1a22888c3659ac6bc64e0ee91578
Andrew-Willms <andrew.e.willms@gmail.com>
2023-04-18 11:51:07 -0400

    Added and set up BluetoothTest project.

---
+++ CyberCavsScoutingSystem/BluetoothTest/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace BluetoothTest;
+
+
+
+public partial class AppShell : Shell {
+
+	public AppShell() {
+		InitializeComponent();
+	}
+
+}
---
+++ CyberCavsScoutingSystem/BluetoothTest/Platforms/Android/MainApplication.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Runtime;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace BluetoothTest;
+
+
+
+[Application]
+public class MainApplication : MauiApplication {
+
+	public MainApplication(nint handle, JniHandleOwnership ownership)
+		: base(handle, ownership) {
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+}
