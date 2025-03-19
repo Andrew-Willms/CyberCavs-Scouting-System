e201d3c02f84d68688f6fda4fc5e446390deeebf
Andrew Willms <andrew.e.willms@gmail.com>
2025-03-19 14:28:11 -0400

    changes

--- CyberCavsScoutingSystem/ScoutingApp/Views/Pages/Flyout/EditMatchPage.xaml.cs
+++ CyberCavsScoutingSystem/ScoutingApp/Views/Pages/Flyout/EditMatchPage.xaml.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
+using CCSSDomain.Data;
+using CCSSDomain.DataCollectors;
 using CCSSDomain.Serialization;
 using Microsoft.Maui.Controls;
+using ScoutingApp.AppManagement;
+using UtilitiesLibrary.Collections;
+using UtilitiesLibrary.Optional;
 
 namespace ScoutingApp.Views.Pages.Flyout;
 
@@ -15,6 +22,8 @@ public partial class EditMatchPage : ContentPage, INotifyPropertyChanged {
 
 	public const string MatchDataNavigationParameterName = nameof(MatchDataDto);
 
+	private IAppManager AppManager { get; }
+
 	public MatchDataDto SavedMatch {
 		get;
 		set {
@@ -24,12 +33,139 @@ public partial class EditMatchPage : ContentPage, INotifyPropertyChanged {
 	} = null!;
 
 
+	public uint? MatchNumber {
 
-	public EditMatchPage() {
+		get => AppManager.ActiveMatchData.MatchNumber.HasValue
+			? AppManager.ActiveMatchData.MatchNumber.Value
+			: null;
+
+		set => AppManager.ActiveMatchData.MatchNumber = value is not null
+			? ((uint)value).Optionalize()
+			: Optional.NoValue;
+	}
+
+	public uint? ReplayNumber {
+
+		get => AppManager.ActiveMatchData.ReplayNumber.HasValue
+			? AppManager.ActiveMatchData.ReplayNumber.Value
+			: null;
+
+		set => AppManager.ActiveMatchData.ReplayNumber = value is not null
+			? ((uint)value).Optionalize()
+			: Optional.NoValue;
+	}
+
+	public MatchType[] MatchTypes { get; } = Enum.GetValues<MatchType>();
+
+	public MatchType? MatchType {
+		get => AppManager.ActiveMatchData.MatchType.HasValue
+			? AppManager.ActiveMatchData.MatchType.Value
+			: null;
+
+		set => AppManager.ActiveMatchData.MatchType = value is not null
+			? ((MatchType)value).Optionalize()
+			: Optional.NoValue;
+	}
+
+	public uint? TeamNumber {
+
+		get => AppManager.ActiveMatchData.TeamNumber.HasValue
+			? AppManager.ActiveMatchData.TeamNumber.Value
+			: null;
+
+		set => AppManager.ActiveMatchData.TeamNumber = value is not null
+			? ((uint)value).Optionalize()
+			: Optional.NoValue;
+	}
+
+	public uint? Alliance {
+		get => AppManager.ActiveMatchData.Alliance.HasValue
+			? AppManager.ActiveMatchData.Alliance.Value
+			: null;
+
+		set => AppManager.ActiveMatchData.Alliance = value is not null && Alliances.Count >= value
+			? ((uint)value).Optionalize()
+			: Optional.NoValue;
+	}
+
+	public ReadOnlyList<string> Alliances => AppManager.ActiveMatchData.GameSpecification.Alliances.Select(x => x.Name).ToReadOnly();
+
+	public ReadOnlyList<InputDataCollector> SetupInputs => AppManager.ActiveMatchData.SetupTabInputs;
+	public ReadOnlyList<InputDataCollector> AutoInputs => AppManager.ActiveMatchData.AutoTabInputs;
+	public ReadOnlyList<InputDataCollector> TeleInputs => AppManager.ActiveMatchData.TeleTabInputs;
+	public ReadOnlyList<InputDataCollector> EndgameInputs => AppManager.ActiveMatchData.EndgameTabInputs;
+	public ReadOnlyList<InputDataCollector> Inputs => SetupInputs.AppendRanges(AutoInputs, TeleInputs, EndgameInputs).ToReadOnly();
+
+	private IErrorPresenter ErrorPresenter { get; }
+
+	public ObservableCollection<string> Errors {
+		get {
+
+			ObservableCollection<string> errors = [];
+
+			if (AppManager.Scout == "") {
+				errors.Add("The scout name has not been set. Go to the Scout page in the flyout menu to set the scout name.");
+			}
+
+			if (AppManager.EventCode == "") {
+				errors.Add("The event has not been set. Go to the Event page in the flyout menu to set the event.");
+			}
+
+			if (AppManager.ActiveMatchData.MatchNumber == Optional.NoValue) {
+				errors.Add("The match number has not been set.");
+			}
+
+			if (AppManager.ActiveMatchData.ReplayNumber == Optional.NoValue) {
+				errors.Add("The replay number has not been set.");
+			}
+
+			// todo figure out how to not have the default MatchType overridden
+			//if (AppManager.ActiveMatchData.MatchType == Optional.NoValue) {
+			//	errors.Add("MatchType has not been set.");
+			//}
+
+			if (AppManager.ActiveMatchData.Alliance == Optional.NoValue) {
+				errors.Add("An alliance has not been set.");
+			}
+
+			if (AppManager.ActiveMatchData.TeamNumber == Optional.NoValue) {
+				errors.Add("The team number has not been set.");
+			}
+
+			foreach (DataField dataField in AppManager.ActiveMatchData.DataFields) {
+				foreach (string error in dataField.Errors) {
+					errors.Add(error);
+				}
+			}
+
+			return errors;
+		}
+	}
+
+	public bool MatchIstValid => Errors.IsEmpty();
+
+
+
+	public EditMatchPage(IAppManager appManager, IErrorPresenter errorPresenter) {
+
+		AppManager = appManager;
+		ErrorPresenter = errorPresenter;
+
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(MatchNumber)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(ReplayNumber)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(MatchType)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(TeamNumber)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(Alliance)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(Alliances)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(SetupInputs)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(AutoInputs)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(TeleInputs)));
+		AppManager.OnMatchStarted.Subscribe(() => OnPropertyChanged(nameof(EndgameInputs)));
 
 		BindingContext = this;
 		InitializeComponent();
 	}
+
 
 
 	// ReSharper disable once AsyncVoidMethod, async void needed for navigation
@@ -40,7 +176,8 @@ public partial class EditMatchPage : ContentPage, INotifyPropertyChanged {
 
 	// ReSharper disable once AsyncVoidMethod, async void needed for navigation
 	private async void CancelButton_OnClicked(object? sender, EventArgs e) {
-		await Shell.Current.GoToAsync($"../{MatchQrCodePage.RouteFromSavedMatchesPage}");
+		await Shell.Current.GoToAsync("..");
+		//await Shell.Current.GoToAsync($"..{MatchQrCodePage.RouteFromSavedMatchesPage}"); // crashes
 	}
 
 
